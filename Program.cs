90a71d966ae94aa57350a877e7e17c0205836c91
a2g2c3d3 <papa3520@naver.com>
2025-07-11 01:43:48 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TextRpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+                Console.WriteLine("원하시는 이름을 설정해주세요.\n\n");
+
+                string playerName = Console.ReadLine();
+
+                Console.WriteLine("\n입력하신 이름은 " + playerName + " 입니다.");
+
+                Console.WriteLine("원하시는 항목을 선택해 주세요.");
+                Console.WriteLine("1. 저장\n2.취소");
+
+                string isYourName = Console.ReadLine();
+                if(int.Parse(isYourName) == 1)
+                {
+                    dd
+                }
+
+                break;
+            }
+
+
+        }
+    }
+}
